c87bbaad6344205506d40710e28237b24fa7c255
Aodh O'Sullivan <aodhsully@gmail.com>
2018-09-14 04:14:56 +0100

    Added CSS to Earthquake Display

--- Earthquake API/sketch.js
+++ Earthquake API/sketch.js
@@ -6,9 +6,6 @@ var cLong = 0;
 var lat = 0;
 var long = 0;
 var zoom = 1;
-// dublin 53.3498° N, 6.2603° W
-// var lat = 53.3498;
-// var long = -6.2603;
 
 function preload() {
   mapImage = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,0/1024x512?access_token=pk.eyJ1IjoiYW9kaCIsImEiOiJjam0wd2c3MGMwNHUyM2tsam15eW1nMmYzIn0.KldvlwYNGivL9pct93Z1xA');
@@ -31,7 +28,8 @@ function mercY(lat) {
 }
 
 function setup() {
-  createCanvas(1024, 512);
+  var canvas = createCanvas(1024, 512);
+  canvas.parent('sketch-holder');
   translate(width / 2, height / 2);
   imageMode(CENTER);
   image(mapImage, 0, 0);
@@ -40,7 +38,6 @@ function setup() {
   var cy = mercY(cLat);
 
   for (var i = 0; i < earthquakes.length; i++) {
-
     (function(i) {
       setTimeout(function() {
 
@@ -48,7 +45,9 @@ function setup() {
         //console.log(data);
         var lat = data[1];
         var long = data[2];
-        var mag = data[4]
+        var mag = data[4];
+        var depth = data[3];
+        var type = data[15];
 
         var x = mercX(long) - cx;
         var y = mercY(lat) - cy;
@@ -58,15 +57,31 @@ function setup() {
 
         var magMax = sqrt(pow(10, 10));
         var d = map(mag, 0, magMax, 2, 500);
-
-        fill(255, 0, 255, 100);
+        // console.log(type);
+        if (type === "explosion" || type === "quarry blast"
+          || type === "chemical" || type === "other event") {
+          fill(255, 255, 255); //white
+        } else if (type === "ice quake") {
+          fill(0, 0, 255); //blue
+        }
+        else {
+          if (depth <= 7) {
+            // } else if (depth > 4 && depth < 7) {
+            fill(0, 255, 0, 50); //green
+          } else if (depth > 7 && depth < 15) {
+            fill(255, 255, 0, 50);
+          } else {
+            fill(255, 0, 0, 100); //red
+          }
+        }
         noStroke();
         ellipse(x, y, d, d)
-
-        document.getElementById("current").innerHTML = i+1;
-
-      }, 1 * i);
+        document.getElementById("current").innerHTML = i + 1;
+      },
+       1000);
+      // slider.value * i);
     })(i);
+    // console.log(slider.value)
   }
 
   document.getElementById("total").innerHTML = earthquakes.length;
