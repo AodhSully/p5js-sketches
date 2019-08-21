9ba0aaafb426005eb912813be10e9720710f0e27
AodhSully <aodhsully@gmail.com>
2019-08-21 20:45:45 +0100

    added sliders and readme

---
+++ Cube wave/ReadMe.md
@@ -0,0 +1,1 @@
+Modulating cube based on [The Coding Train Tutorial](https://thecodingtrain.com/CodingChallenges/086-beesandbombs.html)
--- Cube wave/sketch.js
+++ Cube wave/sketch.js
@@ -2,42 +2,57 @@ let angle = 0;
 let w = 30;
 let ma;
 let maxD;
+let slider;
 
 function setup() {
   createCanvas(500, 500, WEBGL);
   ma = atan(1 / sqrt(2));
   maxD = dist(0, 0, 150, 150);
+
+  waveSize = createSlider(0.6, 2, 1, 0.1);
+  waveSize.style('width', '100px');
+  waveSize.position(10, height-50);
+
+  zoom = createSlider(100, 1000, 500, 50);
+  zoom.style('width', '100px');
+  zoom.position(10, height-25);
+
+  rotateZaxis = createSlider(0, 9.4, 0, 0.1);
+  rotateZaxis.style('width', '100px');
+  rotateZaxis.position(10, height-75);
+
+  cubeWidth = createSlider (0, 20, 2, 1);
+  cubeWidth.style('width', '100px');
+  cubeWidth.position(10, height-100);
+
 }
 
 function draw() {
-  background(100);
-  ortho(-500, 500, 500, -500, 0, 2000); //viewing angle
-
-  rotateX(QUARTER_PI);
-  rotateY(ma);
-  // translate(0, 0, -500);
-  // directionalLight(255, 255, 255, 0, -1, 0);
-  // translate(width /2, height /2);
-  // rectMode(CENTER);
-  // rotateX (angle * 0.3)
-
   let offset = 0;
+  let cubeWidthV = cubeWidth.value();
+  let waveSizeV = waveSize.value();
+  let zoomV = zoom.value();
+  let rotateZaxisV = rotateZaxis.value();
+
+  background(255);
+  ortho(-zoomV, zoomV, zoomV, -zoomV, 0, 2000);
+
+  rotateX(PI / 4);
+  rotateY(ma);
+  rotateZ(rotateZaxisV);
+
   for (let z = 0; z < height; z += w) {
     for (let x = 0; x < width; x += w) {
       push();
       let d = dist(x,z,width/2,height/2);
       let offset = map(d, 0, maxD, -2, 2);
       let a = angle + offset;
-      let h = map(sin(a), -1, 1, 50, 400);
+      let h = map(sin(a), -waveSizeV, waveSizeV, 50, 400);
       translate(x - width /2, 0, z - height / 2);
       normalMaterial();
-      box(w-2, h, w - 2);
+      box(w - cubeWidthV, h, w - cubeWidthV);
       pop();
-      // fill(255);
-      // ambientMaterial(255);
-      // rect(x - width /2 + w / 2, 0, w -2, h);
     }
-    // offset += 0.1;
   }
   angle -= 0.1;
 }
