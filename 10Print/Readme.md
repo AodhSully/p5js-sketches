df7e85c3b9950946262f14df34b98551c1ae0a14
AodhSully <aodhsully@gmail.com>
2019-09-05 16:46:47 +0100

    add readme & interactive sliders

---
+++ 10Print/Readme.md
@@ -0,0 +1,5 @@
+* 10Print in P5.js
+
+Based on the [Coding train Tutorial](https://www.youtube.com/watch?v=bEyTZ5ZZxZs&vl=en)
+
+[10PRINT](http://10print.org/) website.
--- 10Print/sketch.js
+++ 10Print/sketch.js
@@ -1,25 +1,47 @@
-// based on https://www.youtube.com/watch?v=bEyTZ5ZZxZs
-
 let x = 0;
 let y = 0;
-let spacing = 20;
+let slider, spacing, chance;
+
 
 function setup() {
-  createCanvas(400, 400);
+  let canvas = createCanvas(400, 400);
+  canvas.parent("canvas");
+  resetSketch();
+  slider = createSlider(1, 50, 20);
+  slider.position(200, 450);
+  slider.mouseClicked(resetSketch);
+
+  chance = createSlider(0.1, 0.9, 0.5, 0.1);
+  chance.position( 10, 450);
+  chance.mouseClicked(resetSketch);
+  console.log(chance.value());
+}
+
+function resetSketch() {
   background(0);
+  y = 0;
+  x = 0;
 }
 
 function draw() {
-  stroke(255);
-  if (random(1) < 0.5) {
+  spacing = slider.value();
+  stroke(0, 255, 0);
+
+  if (random(1) < chance.value()) {
     line(x, y, x + spacing, y + spacing);
   } else {
     line(x, y + spacing, x + spacing, y);
   }
+
   x = x + spacing;
+
   if (x > width) {
     x = 0;
     y = y + spacing;
   }
 
+  if (y > height) {
+    y = 0;
+    resetSketch();
+  }
 }
